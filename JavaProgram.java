cbed57694c099c3cba3e5487757f385d4923d91a
StSaya <nao.m-lanao@epitech.eu>
2023-12-11 09:56:58 +0400

    exercice 1

---
+++ JavaProgram.java
@@ -0,0 +1,7 @@
+public class JavaProgram {
+
+    public static void main(String[] args) {
+        Vehicle test = new Vehicle("Volkswagen", "Spirocco", 2008);
+        test.describe();
+    }
+}
---
+++ Vehicle.java
@@ -0,0 +1,24 @@
+public class Vehicle {
+    
+    String brand;
+    String model;
+    int year;
+
+    public Vehicle (String b, String m, int y) {
+        brand = b;
+        model = m;
+        year = y;
+    }
+    
+    public void accelerate() {
+        System.out.println("I'm putting the pedal to the metal");
+    }
+
+    public void brake() {
+        System.out.println("Woah! Slow down boy!");
+    }
+
+    public void describe() {
+        System.out.println("I'm a " + brand + " " + model + " made in " + year);
+    }
+}
